078fdb3dcda4835bee4dbc9aef6c1e1f8fd47310
ekkaira <denysxchebotar@gmail.com>
2023-03-22 18:40:26 +0200

    Addded react-router-dom and navigation

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia rerum saepe, cupiditate, eligendi sed blanditiis laboriosam obcaecati magni tempora necessitatibus aliquam eveniet, beatae repellat minus commodi repellendus excepturi debitis? Optio libero pariatur esse reiciendis voluptatibus, distinctio consequuntur, deleniti consequatur expedita id aspernatur est corporis nisi saepe eius, quod facilis atque!</div>
+  )
+}
