9a9d8c5cc2393431a3a4a0fb7960edcef207d39e
ekkaira <denysxchebotar@gmail.com>
2023-03-22 17:56:42 +0200

    Created modal window with backdrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+import { useState } from 'react'
 import { ErrorMessage } from './components/ErrorMessage'
 import { Loader } from './components/Loader'
 import {Product} from './components/Product'
 import { useProducts } from './hooks/products'
+import { Modal } from './components/Modal'
+import { CreateProduct } from './components/CreateProduct'
+import { IProduct } from './models'
 
 
 
 function App() {
 
-  const {loading, error, products} = useProducts()
+  const {loading, error, products, addProduct} = useProducts()
+  const [modal, setModal] = useState(true)
 
+  const createHandler = (product: IProduct) => {
+    setModal(false)
+    addProduct(product)
+  }
  
   return (
-    <div className='container mx-auto max-w-2xl pt-5'>
+    <div className='container mx-auto max-w-2xl pt-5'> 
       {loading && <Loader/>}
       {error && <ErrorMessage error={error}/>}
       {products.map(product => <Product product={product} key={product.id}/>)}
-    </div>
+
+      {modal && <Modal title="Create new product" onClose={() => setModal(false)}>
+        <CreateProduct onCreate={createHandler}/>
+      </Modal>}
+    </div>    
   )
 }
 
--- src/hooks/products.ts
+++ src/hooks/products.ts
@@ -6,8 +6,10 @@ export function useProducts() {
     const [products, setProducts] = useState<IProduct[]>([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState('')
-  
-  
+
+    function addProduct(product: IProduct) {
+        setProducts(prev => [...prev, product])
+    }
   
     async function fetchProducts() {
   
@@ -32,6 +34,6 @@ export function useProducts() {
     }
     , [])
   
-    return{ products, error, loading}
+    return{ products, error, loading, addProduct}
 
 }
